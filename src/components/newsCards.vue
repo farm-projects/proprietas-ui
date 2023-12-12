75cbd0704454d94fee09e2919d7494e9cdf89616
Andre Luiz Mendonca <andreluzz@gmail.com>
2023-12-12 11:46:27 +0000

    News cards component name correction

--- src/components/newsCards.vue
+++ src/components/newsCards.vue
@@ -1,26 +1,27 @@
 <template>
   <div style="display: flex; flex-direction: column; gap: 40px">
-    <card
+    <noticia
       v-for="card in cardsNoticiasSorted"
       :data="card"
       :width="cardWidth"
-    ></card>
+    ></noticia>
   </div>
 </template>
 
 <script setup lang="ts">
-import card from "./newsCard.vue";
+import noticia from "./newsCard.vue";
 
 import { useContentStore } from "../stores/contents";
 import { storeToRefs } from "pinia";
 import { useDisplay } from "vuetify";
 import { computed } from "vue";
 
-const store = useContentStore();
-const { cardsNoticias } = storeToRefs(store);
 const { mobile } = useDisplay();
 
 const cardWidth = computed(() => (mobile.value ? "100%" : "800px"));
+
+const store = useContentStore();
+const { cardsNoticias } = storeToRefs(store);
 
 const cardsNoticiasSorted = computed(() => {
   if (!cardsNoticias.value) {
